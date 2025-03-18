0c83f1551b60d42a2cc02c82e2d47f9f011905d0
Renan Mendes <142457377+ReMendess@users.noreply.github.com>
2025-03-17 22:25:43 -0300

    Add files via upload

---
+++ Trabalho_IA_Cap1.R
@@ -0,0 +1,25 @@
+# Carregar os pacotes necessários
+library(dplyr)
+
+getwd()  # Mostra o diretório atual
+
+# Ler o arquivo CSV
+dados <- read.csv("dados_agricultura.csv", sep=",", header=TRUE)
+
+# Exibir os dados
+print(dados)
+
+head(dados)  # Exibe as primeiras linhas do CSV
+str(dados)   # Mostra a estrutura dos dados
+
+
+# Calcular estatísticas básicas
+media_area <- mean(dados$area, na.rm = TRUE)
+desvio_area <- sd(dados$area, na.rm = TRUE)
+
+# Exibir os resultados
+cat("Média da área plantada:", media_area, "m²\n")
+cat("Desvio padrão da área plantada:", desvio_area, "m²\n")
+
+
+
